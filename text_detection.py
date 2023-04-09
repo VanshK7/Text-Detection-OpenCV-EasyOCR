6e824e72641905d030ea7d15d422e84f61a460f3
Vansh Kharidia <vanshkharidia7@gmail.com>
2023-04-09 23:48:32 +0530

    Removing threshold variable from text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -12,14 +12,12 @@ reader=easyocr.Reader(['en'], gpu=False)            # As CUDA is not available, 
 # Detect Text on Image
 text_=reader.readtext(img)
 
-threshold=0.7
-
 # Draw bbox and text
 for t in text_:
     print(t)
     bbox, text, confidence=t
     print(text)
-    if confidence>threshold:
+    if confidence>0.7:
         cv2.rectangle(img, (int(bbox[0][0]), int(bbox[0][1])), (int(bbox[2][0]), int(bbox[2][1])), (0, 255, 0), 5)
         cv2.putText(img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)        # (image, text, bottom left corner, font, font size, color, thickness)
 
